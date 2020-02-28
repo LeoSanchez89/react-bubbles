5e45177bba7d97d5d879a8ea1a51b2a2db003af6
LeoSanchez89 <58442719+LeoSanchez89@users.noreply.github.com>
2020-02-28 14:12:34 -0500

    login working, edit color working

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -1,20 +1,35 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
 
 const BubblePage = () => {
-  const [colorList, setColorList] = useState([]);
-  // fetch your colors data from the server when the component mounts
+	const [colorList, setColorList] = useState([]);
+	// fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
+  const callColor = () => {
+    axiosWithAuth()
+    .get("/api/colors")
+    .then(res => {
+      // console.log(res.data);
+      setColorList(res.data);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+  }
+  
+	useEffect(() => {
+    callColor();
+	},[]);
 
-  return (
-    <>
-      <ColorList colors={colorList} updateColors={setColorList} />
-      <Bubbles colors={colorList} />
-    </>
-  );
+	return (
+		<>
+      <ColorList colors={colorList} updateColors={setColorList} callColor={callColor}/>
+			<Bubbles colors={colorList} />
+		</>
+	);
 };
 
 export default BubblePage;
--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,89 +1,119 @@
 import React, { useState } from "react";
-import axios from "axios";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
 const initialColor = {
-  color: "",
-  code: { hex: "" }
+	color: "",
+	code: { hex: "" }
 };
+// { colors, updateColors, callColor }
+const ColorList = (props) => {
+	console.log("from colors", props.colors);
+	const [editing, setEditing] = useState(false);
+	const [colorToEdit, setColorToEdit] = useState(initialColor);
 
-const ColorList = ({ colors, updateColors }) => {
-  console.log(colors);
-  const [editing, setEditing] = useState(false);
-  const [colorToEdit, setColorToEdit] = useState(initialColor);
+	const editColor = color => {
+		setEditing(true);
+		setColorToEdit(color);
+	};
 
-  const editColor = color => {
-    setEditing(true);
-    setColorToEdit(color);
-  };
-
-  const saveEdit = e => {
-    e.preventDefault();
+	const saveEdit = e => {
+		e.preventDefault();
     // Make a put request to save your updated color
-    // think about where will you get the id from...
-    // where is is saved right now?
-  };
+    console.log(colorToEdit);
+		// think about where will you get the id from...
+		// where is is saved right now?
+		axiosWithAuth()
+      .put(`/api/colors/${colorToEdit.id}`, {
+        color: colorToEdit.color,
+        code: colorToEdit.code, 
+        id: colorToEdit.id
+      })
+			.then(res => {
+        console.log("from put reqeust", res.data);
+        props.callColor();
+			})
+			.catch(err => {
+				console.log(err);
+			});
+	};
 
-  const deleteColor = color => {
-    // make a delete request to delete this color
-  };
+	const deleteColor = color => {
+		// make a delete request to delete this color
+	};
 
-  return (
-    <div className="colors-wrap">
-      <p>colors</p>
-      <ul>
-        {colors.map(color => (
-          <li key={color.color} onClick={() => editColor(color)}>
-            <span>
-              <span className="delete" onClick={e => {
-                    e.stopPropagation();
-                    deleteColor(color)
-                  }
-                }>
-                  x
-              </span>{" "}
-              {color.color}
-            </span>
-            <div
-              className="color-box"
-              style={{ backgroundColor: color.code.hex }}
-            />
-          </li>
-        ))}
-      </ul>
-      {editing && (
-        <form onSubmit={saveEdit}>
-          <legend>edit color</legend>
-          <label>
-            color name:
-            <input
-              onChange={e =>
-                setColorToEdit({ ...colorToEdit, color: e.target.value })
-              }
-              value={colorToEdit.color}
-            />
-          </label>
-          <label>
-            hex code:
-            <input
-              onChange={e =>
-                setColorToEdit({
-                  ...colorToEdit,
-                  code: { hex: e.target.value }
-                })
-              }
-              value={colorToEdit.code.hex}
-            />
-          </label>
-          <div className="button-row">
-            <button type="submit">save</button>
-            <button onClick={() => setEditing(false)}>cancel</button>
-          </div>
-        </form>
-      )}
-      <div className="spacer" />
+	return (
+		<div className="colors-wrap">
+			<p>colors</p>
+			<ul>
+				{props.colors.map(color => (
+					<li key={color.color} onClick={() => editColor(color)}>
+						<span>
+							<span
+								className="delete"
+								onClick={e => {
+									e.stopPropagation();
+									deleteColor(color);
+								}}
+							>
+								x
+							</span>{" "}
+							{color.color}
+						</span>
+						<div
+							className="color-box"
+							style={{ backgroundColor: color.code.hex }}
+						/>
+					</li>
+				))}
+			</ul>
+			{editing && (
+				<form onSubmit={saveEdit}>
+					<legend>edit color</legend>
+					<label>
+						color name:
+						<input
+							onChange={e =>
+								setColorToEdit({ ...colorToEdit, color: e.target.value })
+							}
+							value={colorToEdit.color}
+						/>
+					</label>
+					<label>
+						hex code:
+						<input
+							onChange={e =>
+								setColorToEdit({
+									...colorToEdit,
+									code: { hex: e.target.value }
+								})
+							}
+							value={colorToEdit.code.hex}
+						/>
+					</label>
+					<div className="button-row">
+						<button type="submit">save</button>
+						<button onClick={() => setEditing(false)}>cancel</button>
+					</div>
+				</form>
+			)}
+			<div className="spacer" />
       {/* stretch - build another form here to add a color */}
-    </div>
-  );
+      	{/* axiosWithAuth()
+      .put(`/api/colors/${colorToEdit.id}`, {
+        color: colorToEdit.color,
+        code: colorToEdit.code, 
+        id: colorToEdit.id
+      })
+			.then(res => {
+        console.log("from put reqeust", res.data);
+        // const newArray = { ...colors, res }
+        updateColors([...colors, res.data]);
+			})
+			.catch(err => {
+				console.log(err);
+			}); */}
+		</div>
+	);
 };
 
 export default ColorList;
